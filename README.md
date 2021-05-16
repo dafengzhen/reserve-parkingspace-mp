304c54be2d9f65b32094a43a6e2451170549ffec
dafengzhen <dafengzhen@aliyun.com>
2021-05-17 04:12:10 +0800

    chore: 初始化项目

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # reserve-parkingspace-mp
+
 预约停车位 - 小程序
+
+# 常用提交类型
+
+```text
+[
+  'build',
+  'chore',
+  'ci',
+  'docs',
+  'feat',
+  'fix',
+  'perf',
+  'refactor',
+  'revert',
+  'style',
+  'test'
+];
+```
+
+https://github.com/conventional-changelog/commitlint/tree/master/@commitlint/config-conventional
---
+++ src/app.jsx
@@ -0,0 +1,27 @@
+import { Component } from "react";
+import { Provider } from "react-redux";
+import "taro-ui/dist/style/index.scss";
+
+import configStore from "./store";
+
+import "./app.css";
+
+const store = configStore();
+
+class App extends Component {
+  componentDidMount() {}
+
+  componentDidShow() {}
+
+  componentDidHide() {}
+
+  componentDidCatchError() {}
+
+  // 在 App 类中的 render() 函数没有实际作用
+  // 请勿修改此函数
+  render() {
+    return <Provider store={store}>{this.props.children}</Provider>;
+  }
+}
+
+export default App;
