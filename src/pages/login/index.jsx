2e460c98759d4e0bc9329763ec0232fba86dfa59
dafengzhen <dafengzhen@aliyun.com>
2021-05-18 17:12:28 +0800

    feat: 编写用户登录页面

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,7 +1,7 @@
 import { Component } from "react";
 import { connect } from "react-redux";
 import { View } from "@tarojs/components";
-
+import { AtButton, AtToast } from "taro-ui";
 import { add, minus, asyncAdd } from "../../actions/counter";
 import Footer from "../../components/Footer";
 
@@ -22,11 +22,43 @@ import Footer from "../../components/Footer";
   })
 )
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      toast: {
+        isOpened: false,
+        status: "",
+        text: "成功预约车位",
+        icon: "check", // close
+      },
+    };
+  }
+
+  handleClick() {}
+
   render() {
     return (
       <View>
-        login
+        <View
+          className="at-row at-row__justify--center at-row__align--center"
+          style={{ height: "600px" }}
+        >
+          <View
+            className="at-col at-col-12"
+            style={{ textAlign: "center", padding: "0 70px" }}
+          >
+            <AtButton onClick={this.handleClick.bind(this)}>用户登录</AtButton>
+          </View>
+        </View>
+
         <Footer current={1} />
+
+        <AtToast
+          isOpened={this.state.toast.isOpened}
+          status={this.state.toast.status}
+          text={this.state.toast.text}
+          icon={this.state.toast.icon}
+        />
       </View>
     );
   }
